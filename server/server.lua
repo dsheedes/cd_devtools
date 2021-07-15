499e1b48f02b28e6af903d8e1f0fcc5f52272453
Ramp <ramp.clash@gmail.com>
2021-07-15 10:04:43 +0100

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -11,7 +11,7 @@ RegisterCommand('restart_script', function(source, args)
         print('^3['..resource_name..'] - Resource Saved^0')
         saved_resource = args[1]
     else
-        print('enter script name in 1st argument ape')
+        print('enter script name in 1st argument')
     end
 end)
 
